bce2a7dc40f5a710db99967bf0929d5938098a06
Yiyang Shi <43549544+yizivan@users.noreply.github.com>
2023-03-31 21:47:40 -0400

    Initial commit

---
+++ HikingTrailApp_IT315/HikingTrail.swift
@@ -0,0 +1,23 @@
+//
+//  HikingTrail.swift
+//  HikingTrailApp_IT315
+//
+//  Created by Vanna Tran on 2/26/23.
+//
+
+import Foundation
+import UIKit
+
+class HikingTrail {
+    var TrailName = ""
+    var TrailDescription = ""
+    var TrailLength = ""
+    var TrailElevation = ""
+    var TrailTime = ""
+    var TrailRouteType = ""
+    var TrailDifficulty = ""
+    var TrailImageName = ""
+    var TrailWebSiteAddress = ""
+    
+}
+
